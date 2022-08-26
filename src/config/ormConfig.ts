02199f3dd2e663194c058b580a8483cf4bcc3185
Alexandre Araujo <alexandre01393@gmail.com>
2022-08-26 18:26:50 -0300

    first deploy

--- src/config/ormConfig.ts
+++ src/config/ormConfig.ts
@@ -11,14 +11,15 @@ export const OrgConfigAsync: TypeOrmModuleAsyncOptions = {
   useFactory: (configService: ConfigService) => ({
     type: 'postgres',
     host: configService.get('POSTGRES_HOST'),
-    port: +configService.get('POSTGRES_PORT'),
-    username: configService.get('POSTGRES_USER'),
-    password: configService.get('POSTGRES_DB_PASSWORD'),
-    database: configService.get('POSTGRES_DATABASE'),
+    url: configService.get('DATABASE_URL'),
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     migrations: [__dirname + '/../migrations/*{.ts,.js}'],
     synchronize: false,
     logging: true,
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
   }),
   dataSourceFactory: async (options) => {
     const dataSource = await new DataSource(options).initialize();
@@ -29,12 +30,13 @@ export const OrgConfigAsync: TypeOrmModuleAsyncOptions = {
 export default new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT,
-  username: process.env.POSTGRES_USER,
-  password: '1234',
-  database: 'mks_backend_challenge',
+  url: process.env.DATABASE_URL,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: false,
   logging: true,
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 });
